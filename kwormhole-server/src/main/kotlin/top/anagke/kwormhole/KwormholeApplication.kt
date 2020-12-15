d794182cc4f5ca28483d1caa57e94707294f64d9
Flandia Yingman <Flandia_YingM@hotmail.com>
2020-12-15 12:29:09 +0800

    build(server): use Kotlin + Gradle + Spring MVC to build KWormhole server
    Language: Kotlin
    Build Tool: Gradle
    Framework: Spring MVC

---
+++ kwormhole-server/src/main/kotlin/top/anagke/kwormhole/KwormholeApplication.kt
@@ -0,0 +1,11 @@
+package top.anagke.kwormhole
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KwormholeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KwormholeApplication>(*args)
+}
---
+++ kwormhole-server/src/test/kotlin/top/anagke/kwormhole/KwormholeApplicationTests.kt
@@ -0,0 +1,13 @@
+package top.anagke.kwormhole
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KwormholeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
