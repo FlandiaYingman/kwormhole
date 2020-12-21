847f35427eb6c20a746259212aec3756a7675c18
Flandia Yingman <Flandia_YingM@hotmail.com>
2020-12-21 17:59:35 +0800

    build(client): use Kotlin + Gradle + TornadoFX & Ktor to build KWormhole client
    Language: Kotlin
    Build Tool: Gradle
    Framework: TornadoFX & Ktor

---
+++ kwormhole-client/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "top.anagke"
+version = "0.0.1"
+
+repositories {
+    maven { url = uri("https://maven.aliyun.com/repository/central") }
+    maven { url = uri("https://maven.aliyun.com/repository/public") }
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+
+    implementation("io.github.microutils:kotlin-logging:2.0.3")
+
+    implementation("no.tornado:tornadofx:1.7.20")
+    implementation("io.ktor:ktor-client-core:1.4.3")
+    implementation("io.ktor:ktor-client-cio:1.4.3")
+    implementation("io.ktor:ktor-client-json:1.4.3")
+    implementation("io.ktor:ktor-client-gson:1.4.3")
+    implementation("io.ktor:ktor-client-mock:1.4.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.compileKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.compileTestKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
