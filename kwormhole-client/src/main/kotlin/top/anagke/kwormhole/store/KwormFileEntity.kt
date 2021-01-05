a41e8afea8166721663456c95dd0046942b2d1e0
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-01-05 20:31:01 +0800

    fix(client): change updateTime to time to unify the field name

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileEntity.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileEntity.kt
@@ -6,5 +6,5 @@ import org.jetbrains.exposed.dao.id.EntityID
 class KwormFileEntity(id: EntityID<String>) : Entity<String>(id) {
     var path by KwormFileTable.path
     val hash by KwormFileTable.hash
-    var updateTime by KwormFileTable.updateTime
+    var time by KwormFileTable.time
 }
--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileTable.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileTable.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.Table
 object KwormFileTable : Table("file_metadata") {
     val path = varchar("path", 255)
     val hash = long("hash")
-    val updateTime = long("update_time")
+    val time = long("time")
 
     override val primaryKey = PrimaryKey(path)
 }
