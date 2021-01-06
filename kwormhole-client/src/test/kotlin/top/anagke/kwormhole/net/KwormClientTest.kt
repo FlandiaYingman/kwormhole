ce143fa7e64add0bc88baa7fc353c200d704f5e3
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-01-06 11:14:41 +0800

    fix(client): fix bugs in the unit test of KwormClient

--- kwormhole-client/src/test/kotlin/top/anagke/kwormhole/net/KwormClientTest.kt
+++ kwormhole-client/src/test/kotlin/top/anagke/kwormhole/net/KwormClientTest.kt
@@ -1,21 +1,19 @@
 package top.anagke.kwormhole.net
 
+import com.google.gson.Gson
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.mock.MockEngine
-import io.ktor.client.engine.mock.MockRequestHandleScope
-import io.ktor.client.engine.mock.respond
 import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.request.HttpRequestData
-import io.ktor.client.request.HttpResponseData
-import io.ktor.content.TextContent
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.PartialContent
 import io.ktor.gson.gson
 import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
-import io.ktor.http.content.OutgoingContent
+import io.ktor.request.contentType
+import io.ktor.request.path
+import io.ktor.request.receive
+import io.ktor.request.receiveText
 import io.ktor.response.respond
 import io.ktor.response.respondBytes
 import io.ktor.routing.param
@@ -28,12 +26,10 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import top.anagke.kwormhole.nextBytesList
 import top.anagke.kwormhole.nextPath
-import top.anagke.kwormhole.store.FileMetadata
 import top.anagke.kwormhole.store.KwormFile
 import top.anagke.kwormhole.store.KwormFile.Companion.utcTimeMillis
-import top.anagke.kwormhole.util.B
 import top.anagke.kwormhole.util.MiB
-import top.anagke.kwormhole.util.hash
+import top.anagke.kwormhole.util.parseRange
 import top.anagke.kwormhole.util.use
 import java.io.File
 import kotlin.random.Random
@@ -48,12 +44,8 @@ internal class KwormClientTest {
     @Test
     fun listFile() {
         val kwormList = List(RANDOM_TIMES) {
-            KwormFile(
-                Random.nextPath(8),
-                FileMetadata(Random.nextLong(), Random.nextLong(0, utcTimeMillis))
-            )
+            KwormFile(Random.nextPath(8), Random.nextLong(), Random.nextLong(0, utcTimeMillis))
         }
-
         val server = embeddedServer(CIO, port = 8080) {
             install(PartialContent)
             install(ContentNegotiation) {
@@ -69,116 +61,123 @@ internal class KwormClientTest {
                 }
             }
         }
-        server.start()
 
+        server.start()
         val kwormClient = KwormClient(TEST_HOST, 8080)
         runBlocking {
-            assertEquals(kwormList, kwormClient.listFiles())
+            assertEquals(kwormList, kwormClient.listFiles(0, Int.MAX_VALUE))
         }
-
         server.stop(0, 0)
     }
 
     @Test
     fun uploadFile() {
-        val contentList = listOf(
-            List(RANDOM_TIMES) { Random.nextBytes(16.B) },
-            List(RANDOM_TIMES) { Random.nextBytes(16.MiB) }
-        ).flatten()
-        val metadataList = List(RANDOM_TIMES * 2) {
-            FileMetadata(contentList[it].hash(), Random.nextLong())
+        val contentList = List(RANDOM_TIMES) { Random.nextBytes(16.MiB) }
+        val kwormList = List(RANDOM_TIMES) {
+            KwormFile(Random.nextPath(8), Random.nextLong(), Random.nextLong(0, utcTimeMillis))
         }
 
         var count = 0
         var prevIsContent = false
-        val handler: suspend MockRequestHandleScope.(request: HttpRequestData) -> HttpResponseData = { request ->
-            println(request)
-            when {
-                request.url.parameters["method"] == "content-ranged" -> {
-                    prevIsContent = true
-                    val range = parseRange(request.headers["Content-Range"]!!)
-                    val body = request.body as OutgoingContent.ByteArrayContent
-                    val expected = contentList[count].sliceArray(range)
-                    val actual = body.bytes()
-                    assertArrayEquals(expected, actual)
-                }
-                request.url.parameters["method"] == "content" -> {
-                    prevIsContent = true
-                    val expected = contentList[count]
-                    val actual = (request.body as OutgoingContent.ByteArrayContent).bytes()
-                    assertArrayEquals(expected, actual)
-                }
-                request.url.parameters["method"] == "metadata" -> {
-                    assertTrue(prevIsContent)
-                    prevIsContent = false
-                    assertEquals(ContentType.Application.Json, request.body.contentType)
-                    val expectedJson = Gson().toJson(metadataList[count])
-                    val actualJson = (request.body as TextContent).text
-                    assertEquals(expectedJson, actualJson)
-                    count++
-                }
-                else -> fail("$request is not content nor metadata")
-            }
-            respond("OK")
-        }
-
-        val kwormClient = KwormClient(TEST_HOST, 8080, HttpClient(MockEngine) {
-            install(JsonFeature)
-            engine {
-                addHandler(handler)
-            }
-        })
-
-        val file = File("test_file")
-        file.deleteOnExit()
-        repeat(RANDOM_TIMES * 2) { i ->
-            file.writeBytes(contentList[i])
-            runBlocking {
-                kwormClient.uploadFile("test_file", file, metadataList[i])
-            }
-        }
-    }
-
-    @Test
-    fun downloadFile() {
-        val contentList = Random.nextBytesList(RANDOM_TIMES, 16.MiB)
-        val metadataList = List(RANDOM_TIMES) {
-            val hash = contentList[it].hash()
-            val updateTime = Random.nextLong(0, utcTimeMillis)
-            FileMetadata(hash, updateTime)
-        }
-
-        var counter = 0
         val server = embeddedServer(CIO, port = 8080) {
             install(PartialContent)
             install(ContentNegotiation) {
                 gson()
             }
             routing {
-                route("/TEST_FILE", HttpMethod.Get) {
+                route("{...}") {
+                    param("method", "content-ranged") {
+                        handle {
+                            val range = parseRange(call.request.headers["Content-Range"]!!)
+                            val expected = contentList[count].sliceArray(range)
+                            val actual = call.receive<ByteArray>()
+                            assertArrayEquals(expected, actual)
+
+                            prevIsContent = true
+                            call.respond("OK")
+                        }
+                    }
                     param("method", "content") {
                         handle {
-                            call.respondBytes(contentList[counter], ContentType.Application.OctetStream)
+                            val expected = contentList[count]
+                            val actual = call.receive<ByteArray>()
+                            assertArrayEquals(expected, actual)
+
+                            prevIsContent = true
+                            call.respond("OK")
                         }
                     }
                     param("method", "metadata") {
                         handle {
-                            call.respond(metadataList[counter++])
+                            val expectedJson = Gson().toJson(kwormList[count])
+                            val actualJson = call.receiveText()
+                            assertTrue(prevIsContent)
+                            assertEquals(ContentType.Application.Json, call.request.contentType())
+                            assertEquals(expectedJson, actualJson)
+
+                            count++
+                            prevIsContent = false
+                            call.respond("OK")
                         }
                     }
                 }
             }
         }
-        server.start()
 
+        server.start()
         val kwormClient = KwormClient(TEST_HOST, 8080, HttpClient {
             install(JsonFeature)
         })
+        TEST_FILE.use {
+            repeat(RANDOM_TIMES) { i ->
+                runBlocking {
+                    TEST_FILE.writeBytes(contentList[i])
+                    kwormClient.uploadFile(kwormList[i], TEST_FILE)
+                }
+            }
+        }
+        server.stop(0, 0)
+    }
 
+    @Test
+    fun downloadFile() {
+        val contentList = Random.nextBytesList(RANDOM_TIMES, 16.MiB)
+        val kwormList = List(RANDOM_TIMES) {
+            KwormFile(Random.nextPath(8), Random.nextLong(), Random.nextLong(0, utcTimeMillis))
+        }
+
+        val server = embeddedServer(CIO, port = 8080) {
+            install(PartialContent)
+            install(ContentNegotiation) {
+                gson()
+            }
+            routing {
+                route("{...}") {
+                    param("method", "content") {
+                        handle {
+                            call.respondBytes(
+                                contentList[kwormList.indexOfFirst { it.path == call.request.path() }],
+                                ContentType.Application.OctetStream
+                            )
+                        }
+                    }
+                    param("method", "metadata") {
+                        handle {
+                            call.respond(kwormList.find { it.path == call.request.path() }!!)
+                        }
+                    }
+                }
+            }
+        }
+
+        server.start()
+        val kwormClient = KwormClient(TEST_HOST, 8080, HttpClient {
+            install(JsonFeature)
+        })
         repeat(RANDOM_TIMES) {
             TEST_FILE.use { _ ->
                 runBlocking {
-                    kwormClient.downloadFile("TEST_FILE", TEST_FILE)
+                    kwormClient.downloadFile(kwormList[it].path, TEST_FILE)
                     val expectedBytes = contentList[it]
                     val actualBytes = TEST_FILE.readBytes()
                     assertArrayEquals(expectedBytes, actualBytes)
