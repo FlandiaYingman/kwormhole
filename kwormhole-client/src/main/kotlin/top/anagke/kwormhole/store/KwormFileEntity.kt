fb0bc7303d928deb11dcc01fdc601278339c136e
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-01-29 18:20:41 +0800

    fix(client): add companion util object

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileEntity.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileEntity.kt
@@ -5,6 +5,8 @@ import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
 class KwormFileEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<KwormFileEntity>(KwormFileTable)
+
     var path by KwormFileTable.path
     var status by KwormFileTable.status
     var hash by KwormFileTable.hash
