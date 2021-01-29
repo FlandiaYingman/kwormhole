1c099f815c1063f6c9108de04e7b35a2666658a8
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-01-29 18:13:30 +0800

    fix(client): use var for all fields

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileEntity.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/store/KwormFileEntity.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.dao.id.EntityID
 
 class KwormFileEntity(id: EntityID<String>) : Entity<String>(id) {
     var path by KwormFileTable.path
-    val status by KwormFileTable.status
-    val hash by KwormFileTable.hash
+    var status by KwormFileTable.status
+    var hash by KwormFileTable.hash
     var time by KwormFileTable.time
 }
