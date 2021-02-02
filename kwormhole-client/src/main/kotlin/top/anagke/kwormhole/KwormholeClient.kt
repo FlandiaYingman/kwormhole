b1027a6e9b2b4aa0589d659a326158b0e27fcbe3
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-02-02 19:44:43 +0800

    feat(client): add CLI support

---
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/KwormholeClient.kt
@@ -0,0 +1,24 @@
+package top.anagke.kwormhole
+
+import com.xenomachina.argparser.ArgParser
+import top.anagke.kwormhole.net.KwormClient
+import top.anagke.kwormhole.store.LocalStore
+import top.anagke.kwormhole.store.RemoteStore
+import top.anagke.kwormhole.sync.KwormSync
+import java.io.File
+
+fun main(argv: Array<String>) {
+    val args = ArgParser(argv).parseInto(::KwormholeClientArgs)
+    val localStore = LocalStore(File(args.root))
+    val remoteStore = RemoteStore(KwormClient(args.serverHost, args.serverPort))
+    val sync = KwormSync(localStore, remoteStore)
+    while (!Thread.interrupted()) {
+        localStore.index()
+        for (m in localStore.list()) {
+            println("Indexed: $m")
+        }
+
+        sync.sync()
+        Thread.sleep(3000)
+    }
+}
