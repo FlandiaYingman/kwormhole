0c39908d026a72538957cd5a7636d34f5c180076
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-02-02 10:11:14 +0800

    feat(client): the simple implementation of KwormClient

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/net/KwormClient.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/net/KwormClient.kt
@@ -1,20 +1,26 @@
+@file:Suppress("EXPERIMENTAL_API_USAGE")
+
 package top.anagke.kwormhole.net
 
+import com.google.gson.Gson
 import io.ktor.client.HttpClient
 import io.ktor.client.call.receive
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.delete
+import io.ktor.client.request.forms.InputProvider
+import io.ktor.client.request.forms.MultiPartFormDataContent
+import io.ktor.client.request.forms.formData
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
-import io.ktor.client.request.put
-import io.ktor.client.statement.HttpStatement
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import io.ktor.utils.io.ByteReadChannel
-import top.anagke.kwormhole.store.KwormFile
-import top.anagke.kwormhole.util.MiB
-import top.anagke.kwormhole.util.contentRange
-import top.anagke.kwormhole.util.suspendForEachBlock
-import top.anagke.kwormhole.util.suspendReadBytes
+import io.ktor.client.request.post
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.content.MultiPartData
+import io.ktor.http.content.PartData
+import io.ktor.utils.io.core.copyTo
+import io.ktor.utils.io.streams.asInput
+import io.ktor.utils.io.streams.asOutput
+import top.anagke.kwormhole.store.Metadata
+import top.anagke.kwormhole.util.fromJson
 import java.io.File
 
 class KwormClient(
@@ -25,101 +31,44 @@ class KwormClient(
     }
 ) {
 
-    /**
-     * Counts the files in the remote.
-     * @return the count of files
-     */
-    suspend fun countFiles(): Int {
-        return httpClient.get(host = host, port = port, path = "/count") {
-        }
+    suspend fun listFiles(): List<Metadata> {
+        return httpClient.get(host = host, port = port, path = "/")
     }
 
-
-    /**
-     * Lists all files in the remote.
-     * @param start the starting index of listing
-     * @param limit the limit of listing
-     * @return the file list
-     */
-    suspend fun listFiles(start: Int, limit: Int): List<KwormFile> {
-        return httpClient.get(host = host, port = port, path = "/list") {
-            parameter("start", start)
-            parameter("limit", limit)
-        }
-    }
-
-
-    /**
-     * Downloads a remote file to a local file.
-     * @param path the path of the remote file
-     * @param file the local file
-     * @return the metadata of the remote file
-     */
-    suspend fun downloadFile(path: String, file: File): KwormFile {
-        httpClient.get<HttpStatement>(host = host, port = port, path = path) {
-            parameter("method", "content")
-        }.execute {
-            val size = 4.MiB
-            val buffer = ByteArray(size)
-            val channel = it.receive<ByteReadChannel>()
-            var read = 0
-            file.outputStream().use {
-                do {
-                    it.write(buffer, 0, read)
-                    read = channel.readAvailable(buffer, 0, size)
-                } while (read >= 0)
-            }
-        }
+    suspend fun peek(path: String): Metadata? {
         return httpClient.get(host = host, port = port, path = path) {
-            parameter("method", "metadata")
+            parameter("peek", "")
         }
     }
 
+    suspend fun downloadFile(path: String, file: File): Metadata {
+        val response = httpClient.get<HttpResponse>(host = host, port = port, path = path)
+        val parts = response.receive<MultiPartData>()
+        val metadataPart = (parts.readPart() ?: throw RuntimeException("Server error")) as PartData.FormItem
+        val contentPart = (parts.readPart() ?: throw RuntimeException("Server error")) as PartData.FileItem
 
-    /**
-     * Uploads a local file to remote.
-     * @param metadata the metadata of the local file
-     * @param file the path of the local file
-     */
-    suspend fun uploadFile(metadata: KwormFile, file: File) {
-        when {
-            file.length() <= 4.MiB ->
-                uploadSmallFile(metadata, file)
-            else ->
-                uploadLargeFile(metadata, file)
-        }
-    }
-
-    private suspend fun uploadSmallFile(metadata: KwormFile, file: File) {
-        httpClient.put<Unit>(host = host, port = port, path = metadata.path) {
-            contentType(ContentType.Application.OctetStream)
-            parameter("method", "content")
-            body = file.suspendReadBytes()
-        }
-        httpClient.put<Unit>(host = host, port = port, path = metadata.path) {
-            contentType(ContentType.Application.Json)
-            parameter("method", "metadata")
-            body = metadata
-        }
-    }
-
-    private suspend fun uploadLargeFile(metadata: KwormFile, file: File) {
-        var totalRead = 0
-        val fileLength = file.length()
-        file.suspendForEachBlock(4.MiB) { buffer, read ->
-            httpClient.put<Unit>(host = host, port = port, path = metadata.path) {
-                contentType(ContentType.Application.OctetStream)
-                contentRange(totalRead until read + totalRead, fileLength)
-                parameter("method", "content-ranged")
-                body = buffer
+        val metadata = Gson().fromJson<Metadata>(metadataPart.value)!!
+        contentPart.provider().use { input ->
+            file.outputStream().asOutput().use { output ->
+                input.copyTo(output)
             }
-            totalRead += read
         }
-        httpClient.put<Unit>(host = host, port = port, path = metadata.path) {
-            contentType(ContentType.Application.Json)
-            parameter("method", "metadata")
-            body = metadata
+        return metadata
+    }
+
+    suspend fun uploadFile(metadata: Metadata, file: File) {
+        httpClient.post<Unit>(host = host, port = port, path = metadata.path) {
+            body = MultiPartFormDataContent(
+                formData {
+                    this.append("metadata", Gson().toJson(metadata))
+                    this.append("content", InputProvider(file.length()) { file.inputStream().asInput() })
+                }
+            )
         }
+    }
+
+    suspend fun deleteFile(path: String) {
+        httpClient.delete<Unit>(host = host, port = port, path = path)
     }
 
 }
