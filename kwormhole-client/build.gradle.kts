4261a717cd3c758b50cc37f6bd4ae27696b5232e
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-03-13 23:03:35 +0800

    build: update kotlin and ktor

--- kwormhole-client/build.gradle.kts
+++ kwormhole-client/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.4.31"
     id("com.github.ben-manes.versions") version "0.36.0"
 }
 
@@ -26,14 +26,14 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging:2.0.4")
 
     implementation("no.tornado:tornadofx:1.7.20")
-    implementation("io.ktor:ktor-client-core:1.5.0")
-    implementation("io.ktor:ktor-client-cio:1.5.0")
-    implementation("io.ktor:ktor-client-json:1.5.0")
-    implementation("io.ktor:ktor-client-gson:1.5.0")
-    implementation("io.ktor:ktor-client-mock:1.5.0")
-    implementation("io.ktor:ktor-server-core:1.5.0")
-    implementation("io.ktor:ktor-server-cio:1.5.0")
-    implementation("io.ktor:ktor-gson:1.5.0")
+    implementation("io.ktor:ktor-client-core:1.5.1")
+    implementation("io.ktor:ktor-client-cio:1.5.1")
+    implementation("io.ktor:ktor-client-json:1.5.1")
+    implementation("io.ktor:ktor-client-gson:1.5.1")
+    implementation("io.ktor:ktor-client-mock:1.5.1")
+    implementation("io.ktor:ktor-server-core:1.5.1")
+    implementation("io.ktor:ktor-server-cio:1.5.1")
+    implementation("io.ktor:ktor-gson:1.5.1")
 
     implementation("org.jetbrains.exposed:exposed-core:0.29.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.29.1")
