37e38fdd751882808e636aef942d306eb3e28386
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-04-28 15:32:45 +0800

    feat: simple bootstrap function

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/KwormholeClient.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/KwormholeClient.kt
@@ -1,24 +1,48 @@
 package top.anagke.kwormhole
 
 import com.xenomachina.argparser.ArgParser
-import top.anagke.kwormhole.net.KwormClient
-import top.anagke.kwormhole.store.LocalStore
-import top.anagke.kwormhole.store.RemoteStore
-import top.anagke.kwormhole.sync.KwormSync
+import top.anagke.kwormhole.model.LocalModel
+import top.anagke.kwormhole.model.RecordDatabase
+import top.anagke.kwormhole.model.RemoteModel
+import top.anagke.kwormhole.sync.Synchronizer
 import java.io.File
+import java.net.InetSocketAddress
 
-fun main(argv: Array<String>) {
-    val args = ArgParser(argv).parseInto(::KwormholeClientArgs)
-    val localStore = LocalStore(File(args.root))
-    val remoteStore = RemoteStore(KwormClient(args.serverHost, args.serverPort))
-    val sync = KwormSync(localStore, remoteStore)
-    while (!Thread.interrupted()) {
-        localStore.index()
-        for (m in localStore.listAll()) {
-            println("Indexed: $m")
-        }
+fun main(args: Array<String>) {
+    val parsedArgs = ArgParser(args).parseInto(::Args)
 
-        sync.sync()
-        Thread.sleep(3000)
+    val database = RecordDatabase(File("./kwormhole.db"))
+    val localModel = LocalModel(File(parsedArgs.root), database)
+    val remoteModel = RemoteModel(InetSocketAddress(parsedArgs.serverHost, parsedArgs.serverPort))
+
+    val uploader = Synchronizer(localModel, remoteModel)
+    val downloader = Synchronizer(remoteModel, localModel)
+
+    return
+}
+
+private class Args(parser: ArgParser) {
+
+    val root by parser.storing(
+        "-r", "--root",
+        help = "The root directory of Kwormhole Client."
+    )
+
+    val server by parser.storing(
+        "-s", "--server",
+        help = "The address of Kwormhole Server"
+    )
+
+    val serverHost by lazy {
+        server.split(":")[0]
     }
+
+    val serverPort by lazy {
+        server.split(":")[1].toInt()
+    }
+
+    override fun toString(): String {
+        return "Args(root='$root', serverHost='$serverHost', serverPort=$serverPort)"
+    }
+
 }
