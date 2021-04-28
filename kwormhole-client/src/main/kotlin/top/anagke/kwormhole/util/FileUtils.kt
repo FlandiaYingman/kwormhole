98584faeed57c4bc0e09344427f50c816ca63453
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-04-28 15:17:18 +0800

    fix: remove redundant files

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/util/FileUtils.kt
+++
@@ -1,7 +0,0 @@
-package top.anagke.kwormhole.util
-
-import java.io.File
-
-infix fun File.matches(file: File): Boolean {
-    return this.canonicalPath == file.canonicalPath
-}
--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/util/StringUtils.kt
+++
@@ -1,5 +0,0 @@
-package top.anagke.kwormhole.util
-
-fun String.splitPair(delimiter: String): Pair<String, String> {
-    return this.substringBefore(delimiter) to this.substringAfter(delimiter)
-}
