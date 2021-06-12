e7a6c09340c7d238053915d2efcdca71da3a1367
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-06-13 01:00:27 +0800

    fix: every watch event is resolved by the original root

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/model/local/FileAltMonitor.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/model/local/FileAltMonitor.kt
@@ -7,6 +7,7 @@ import java.nio.file.FileSystems
 import java.nio.file.Path
 import java.nio.file.StandardWatchEventKinds.*
 import java.nio.file.WatchEvent
+import java.nio.file.WatchKey
 import java.nio.file.WatchService
 import java.util.concurrent.BlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
@@ -36,7 +37,7 @@ class FileAltMonitor(
         register(root)
         while (!Thread.interrupted()) {
             val key = service.take()
-            key.pollEvents().forEach(this::submit)
+            key.pollEvents().forEach { submit(key, it) }
             key.reset()
         }
     }
@@ -48,10 +49,14 @@ class FileAltMonitor(
             .forEach { it.toPath().register(service, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY) }
     }
 
-    private fun submit(event: WatchEvent<*>) {
-        val file = root.resolve((event.context() as Path).toFile())
-        if (file.isDirectory) register(file)
-        alternations.put(file)
+    private fun submit(key: WatchKey, event: WatchEvent<*>) {
+        val parent = (key.watchable() as Path).toFile()
+        val file = parent.resolve((event.context() as Path).toFile())
+        if (file.isDirectory) {
+            register(file)
+        } else {
+            alternations.put(file)
+        }
     }
 
     fun take(): List<File> {
