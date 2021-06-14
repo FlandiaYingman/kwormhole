85340f49887c57a6662f7ec9d215de3b9a92f7ae
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-06-15 01:15:02 +0800

    refactor: delete unused

--- kwormhole-client/src/main/kotlin/top/anagke/kwormhole/util/KotlinGson.kt
+++ kwormhole-client/src/main/kotlin/top/anagke/kwormhole/util/KotlinGson.kt
@@ -6,4 +6,3 @@ import com.google.gson.reflect.TypeToken
 
 inline fun <reified T> Gson.fromJson(json: String): T? = fromJson(json, object : TypeToken<T>() {}.type)
 
-inline fun <reified T : List<T>> Gson.listFromJson(json: String): T? = this.fromJson(json)
--- kwormhole-client/src/test/kotlin/top/anagke/kwormhole/model/ModelTests.kt
+++
@@ -1,6 +0,0 @@
-package top.anagke.kwormhole.model
-
-import top.anagke.kio.bytes
-import top.anagke.kwormhole.Kfr
-import top.anagke.kwormhole.parseKfrPath
-import top.anagke.kwormhole.test.TEST_DIR
